9b06fca034c11bb79a61f727645d64e44a01f677
Dev7570 <devggre@gmail.com>
2026-03-17 09:38:43 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,6 @@
+// Firebase disabled for local preview
+
+export const auth = null;
+export const db = null;
+export const loginWithGoogle = () => Promise.resolve();
+export const logout = () => Promise.resolve();
